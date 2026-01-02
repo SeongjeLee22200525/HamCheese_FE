d927c21351ee3dea9c27d03edfae48535c6d9620
이성제 <seongjei646@gmail.com>
2026-01-02 13:19:00 +0900

    컴포넌트 구조 최신화

--- src/pages/joinmc.tsx
+++ src/pages/joinmc.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useRef, useState } from "react";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/components/common/Header";
+import Footer from "@/components/common/Footer";
 
 export default function JoinMC() {
   const departments = [
