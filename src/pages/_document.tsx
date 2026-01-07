7509b965d99efadc5f4cb0722811187d4871f7f4
이성제 <seongjei646@gmail.com>
2026-01-07 15:59:26 +0900

    파비콘 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* 🔥 파비콘 강제 선언 (캐시 무효화 포함) */}
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/favicon.svg?v=20260107"
+        />
+        <link
+          rel="alternate icon"
+          href="/favicon.ico?v=20260107"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
